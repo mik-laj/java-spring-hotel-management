1f11f60a2c625ebfd9ad6a70a68c4c0ecd3f4cb4
Plotron <piotr.graniszewski@op.pl>
2018-01-11 23:23:44 +0100

    Refactor

--- src/main/java/com/teamknp/hotel/config/StaticResourceLocationConfiguration.java
+++ src/main/java/com/teamknp/hotel/config/StaticResourceLocationConfiguration.java
@@ -1,21 +1,16 @@
 package com.teamknp.hotel.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
-public class StaticResourceLocationConfiguration {
-    @Bean
-    WebMvcConfigurer configurer () {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addResourceHandlers (ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("/**").
-                        addResourceLocations("classpath:/static/");
-            }
-        };
+@Component
+public class StaticResourceLocationConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").
+                addResourceLocations("classpath:/dupa/");
     }
 }
