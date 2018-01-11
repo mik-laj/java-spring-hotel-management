7a30af228228f9022429d17f9e77b192c6bbe63a
Plotron <Plotron@users.noreply.github.com>
2018-01-11 23:50:18 +0100

    Adds custom static content location configuration (#5)
    * Static content location configuration
    
    * Refactor
    
    * Refactor

---
+++ src/main/java/com/teamknp/hotel/config/StaticResourceLocationConfiguration.java
@@ -0,0 +1,16 @@
+package com.teamknp.hotel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Component
+public class StaticResourceLocationConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").
+                addResourceLocations("classpath:/static/");
+    }
+}
