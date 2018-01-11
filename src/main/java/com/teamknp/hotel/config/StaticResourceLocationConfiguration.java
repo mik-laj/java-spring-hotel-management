81d8a7738cea3aab7789b77f21816ca4819ca3c0
Plotron <piotr.graniszewski@op.pl>
2018-01-11 23:05:46 +0100

    Static content location configuration

---
+++ src/main/java/com/teamknp/hotel/config/StaticResourceLocationConfiguration.java
@@ -0,0 +1,21 @@
+package com.teamknp.hotel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class StaticResourceLocationConfiguration {
+    @Bean
+    WebMvcConfigurer configurer () {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addResourceHandlers (ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/**").
+                        addResourceLocations("classpath:/static/");
+            }
+        };
+    }
+}
