63d703ef3f6a9c5f1ef5d43e382ec3e04948c9f7
mik-laj <kamil.bregula@siecobywatelska.pl>
2018-01-12 05:16:24 +0100

    Fix invalid indendation

--- src/main/java/com/teamknp/hotel/config/StaticResourceLocationConfiguration.java
+++ src/main/java/com/teamknp/hotel/config/StaticResourceLocationConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class StaticResourceLocationConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-               registry.addResourceHandler("/**").
-                               addResourceLocations("classpath:/static/");
-            }
+        registry.addResourceHandler("/**").
+                addResourceLocations("classpath:/static/");
+    }
 }
