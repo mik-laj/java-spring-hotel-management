b2ea8a1be15511f5540f7b3bdbb6708d412d0c64
Plotron <Plotron@users.noreply.github.com>
2018-01-19 14:56:44 +0100

    Fully working (TM) base layout (#10)

--- src/main/java/com/teamknp/hotel/controller/LoginController.java
+++ src/main/java/com/teamknp/hotel/controller/LoginController.java
@@ -5,11 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-        @GetMapping("/")
-        public String home1() {
-            return "/home";
-        }
-
         @GetMapping("/home")
         public String home() {
             return "/home";
