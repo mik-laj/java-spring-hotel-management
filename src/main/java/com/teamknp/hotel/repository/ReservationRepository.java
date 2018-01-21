521a8be53561e78fd524c62ed9fec9ae3fbe4abd
Plotron <piotr.graniszewski@op.pl>
2018-01-21 15:46:17 +0100

    Scheduled midnight database reservation status update + LocalDate -> sql.Date converters

--- src/main/java/com/teamknp/hotel/repository/ReservationRepository.java
+++ src/main/java/com/teamknp/hotel/repository/ReservationRepository.java
@@ -4,12 +4,19 @@ import com.teamknp.hotel.entity.Reservation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
     @Query("select r from Reservation r inner join r.client as c where c.firstName LIKE :query or c.lastName LIKE :query")
     Page<Reservation> search(@Param("query") String query, Pageable pageable);
+
+    @Modifying
+    @Query(value = "update Reservation r set r.status = 'TIMED_OUT' where r.status = 'PENDING' and r.startDate < :timeoutDate")
+    void timeoutReservationStatuses(@Param("timeoutDate")LocalDate timeoutDate);
 }
